a94b7273ba08e7b76658bde7d2cd798dcea9dd1e
Renato Coelho <t.renato.coelho@lwsa.tech>
2025-02-24 09:18:40 -0300

    ignore /poc-coelho/healthcheck

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 
-  app.use(AWSXRay.express.closeSegment());  
+  app.use(AWSXRay.express.closeSegment());   
 }
 bootstrap();
